2f8be61b249c13b8806521db07976d2dc6684b11
Eric Dugre <ericd@kentico.com>
2022-10-06 19:54:37 -0400

    add tests

---
+++ tests/Fakes/MockEventLogService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+using CMS.Core;
+using CMS.EventLog;
+
+namespace Kentico.Xperience.Algolia.Test
+{
+    internal class MockEventLogService : EventLogService
+    {
+        public List<EventLogData> LoggedEvents => new List<EventLogData>();
+
+
+        public override void LogEvent(EventLogData eventLogData)
+        {
+            LoggedEvents.Add(eventLogData);
+        }
+    }
+}
---
+++ tests/Tests/IAlgoliaIndexServiceTests.cs
@@ -0,0 +1,91 @@
+﻿using System;
+
+using Algolia.Search.Clients;
+
+using Kentico.Xperience.Algolia.Models;
+using Kentico.Xperience.Algolia.Services;
+
+using NSubstitute;
+
+using NUnit.Framework;
+
+namespace Kentico.Xperience.Algolia.Test
+{
+    internal class IAlgoliaIndexServiceTests
+    {
+        [TestFixture]
+        internal class GetIndexSettingsTests : AlgoliaTest
+        {
+            private DefaultAlgoliaIndexService algoliaIndexService;
+
+
+            [SetUp]
+            public void GetIndexSettingsTestsSetUp()
+            {
+                algoliaIndexService = new DefaultAlgoliaIndexService(Substitute.For<ISearchClient>());
+            }
+
+
+            [TestCase(typeof(TestSearchModels.ArticleEnSearchModel), ExpectedResult = new string[] { "searchable(FacetableProperty)", "searchable(ClassName)", "filterOnly(DocumentPublishFrom)", "filterOnly(DocumentPublishTo)" })]
+            public string[] GetIndexSettings_AttributesForFaceting_PropertiesConvertedToArray(Type searchModel)
+            {
+                var algoliaIndex = new AlgoliaIndex(searchModel, nameof(searchModel));
+
+                return algoliaIndexService.GetIndexSettings(algoliaIndex).AttributesForFaceting.ToArray();
+            }
+
+
+            [TestCase(typeof(TestSearchModels.ProductsSearchModel), ExpectedResult = new string[] { "RetrievableProperty", "ObjectID", "ClassName", "Url" })]
+            public string[] GetIndexSettings_AttributesToRetrieve_PropertiesConvertedToArray(Type searchModel)
+            {
+                var algoliaIndex = new AlgoliaIndex(searchModel, nameof(searchModel));
+
+                return algoliaIndexService.GetIndexSettings(algoliaIndex).AttributesToRetrieve.ToArray();
+            }
+
+
+            [Test]
+            public void GetIndexSettings_DistinctOptions_ReturnsOptions()
+            {
+                var algoliaIndex = IndexStore.Instance.Get(nameof(TestSearchModels.SplittingModel));
+                var indexSettings = algoliaIndexService.GetIndexSettings(algoliaIndex);
+
+                Assert.Multiple(() => {
+                    Assert.That(indexSettings.AttributeForDistinct, Is.EqualTo(nameof(TestSearchModels.SplittingModel.AttributeForDistinct)));
+                    Assert.That(indexSettings.Distinct, Is.EqualTo(1));
+                });
+            }
+
+
+            [TestCase(typeof(TestSearchModels.ArticleEnSearchModel), ExpectedResult = new string[] { "unordered(UnorderedProperty)", "NodeAliasPath" })]
+            [TestCase(typeof(TestSearchModels.ProductsSearchModel), ExpectedResult = new string[] { "Order1Property1,Order1Property2", "Order2Property", "NodeAliasPath" })]
+            public string[] GetIndexSettings_SearchableAttributes_PropertiesConvertedToArray(Type searchModel)
+            {
+                var algoliaIndex = new AlgoliaIndex(searchModel, nameof(searchModel));
+
+                return algoliaIndexService.GetIndexSettings(algoliaIndex).SearchableAttributes.ToArray();
+            }
+        }
+
+
+        [TestFixture]
+        internal class InitializeIndexTests : AlgoliaTest
+        {
+            private IAlgoliaIndexService algoliaIndexService;
+
+
+            [SetUp]
+            public void InitializeIndexTestsTestsSetUp()
+            {
+                algoliaIndexService = new DefaultAlgoliaIndexService(Substitute.For<ISearchClient>());
+            }
+
+
+            [Test]
+            public void InitializeIndex_InvalidIndex_Throws()
+            {
+                Assert.Throws<InvalidOperationException>(() => algoliaIndexService.InitializeIndex("NO_INDEX"));
+            }
+        }
+    }
+}
